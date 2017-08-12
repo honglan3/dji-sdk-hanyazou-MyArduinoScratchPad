743ce54d87aef3a89f8fe4485a0c4ecba132bee2
Zou Hanya <hanyazou@gmail.com>
2017-08-13 08:28:14 +0900

    Rename HPDL-1414-gpio_expander-debug.ino to HPDL-1414-gpio_expander.ino

--- HPDL-1414/HPDL-1414-gpio_expander/HPDL-1414-gpio_expander.ino
+++ HPDL-1414/HPDL-1414-gpio_expander/HPDL-1414-gpio_expander.ino
@@ -2,43 +2,55 @@
 #include <mcp23017.h>
 
 #define I2C_ADDR 0x20
-mcp23017 mygpio;
+#define N 4
+mcp23017 mygpio[N];
+int nunits = 0;
 
 void setup(){
-  Serial.begin(38400);
+  while (!Serial);
+  Serial.begin(9600);
   Serial.println("HPDL-1414 test...");
 
   Wire.begin();
-  Wire.beginTransmission (I2C_ADDR);
-  if (Wire.endTransmission () != 0) {
-    Serial.print("ERROR: I2C addr ");
-    Serial.print(I2C_ADDR);
-    Serial.println(" not responding");
-  }
 
-  mygpio.postSetup(I2C_ADDR);
-  mygpio.begin();
-  mygpio.gpioPinMode(OUTPUT);
+  for (int i = 0; i < N; i++){
+    Wire.beginTransmission (I2C_ADDR + i);
+    if (Wire.endTransmission () == 0) {
+      Serial.print("Unit ");
+      Serial.print(i);
+      Serial.print(" on I2C addr ");
+      Serial.println(I2C_ADDR + i);
+      mygpio[nunits].postSetup(I2C_ADDR + i);
+      mygpio[nunits].begin();
+      mygpio[nunits].gpioPinMode(OUTPUT);
+      nunits++;
+    }
+  }
 }
 
 void hpdl1414_put(int pos, uint8_t data)
 {
   const uint16_t ctrl = 0xfc;
+  int unit = nunits-1 - pos/16;
+  if (unit < 0) {
+    return;
+  }
+  pos %= 16;
   int _wr = ~(0x4 << (pos / 4));
   pos %= 4;
 
-  mygpio.gpioPort(data, ctrl | pos);
+  mygpio[unit].gpioPort(data, ctrl | pos);
   delayMicroseconds(1);  // tWD   Write Delay Time  20ns 
-  mygpio.gpioPort(data, (ctrl & _wr) | pos);
+  mygpio[unit].gpioPort(data, (ctrl & _wr) | pos);
   delayMicroseconds(1);  // tW    Write Time  130ns
-  mygpio.gpioPort(data, ctrl | pos);
+  mygpio[unit].gpioPort(data, ctrl | pos);
   delayMicroseconds(1);  // tAH, tDH  Address/Data Hold Time  50ns
 }
 
 void loop(){
-  for (int c = 0; c < 64; c++) {
-    for (int i = 0; i < 8; i++) {
-      hpdl1414_put(i, (c + 7 - i) % 64 + 32);
+  for (int c = 0; c < 64 + 16 * nunits; c++) {
+    for (int i = 0; i < 16 * nunits; i++) {
+      hpdl1414_put(i, (c + 7 - i) + 32);
     }
     delay(50);
   }
