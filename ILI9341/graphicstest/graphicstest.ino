3220eccf49844242603c85edb2362f7b6dc492c7
Zou Hanya <hanyazou@gmail.com>
2017-12-30 19:04:42 +0900

    Change D/C pin assign and add Reset pin

--- ILI9341/graphicstest/graphicstest.ino
+++ ILI9341/graphicstest/graphicstest.ino
@@ -19,11 +19,12 @@
 #include "Adafruit_ILI9341.h"
 
 // For the Adafruit shield, these are the default.
-#define TFT_DC 9
+#define TFT_DC 7
+#define TFT_RST 8
 #define TFT_CS 10
 
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
 // If using the breakout, change pins as desired
 //Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
